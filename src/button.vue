e356f221c464f23b40df9fbcec1b16c210bdc75d
Ambler <allinlbj@gmail.com>
2019-05-09 10:29:13 +0800

    自闭合标签

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <!-- 安装书写规范要求，这得要自闭合，但是自闭合之后后边的content无法渲染，或许这是工具出问题的缘故-->
+    <!-- <g-icon class="icon" v-if="icon" :name="icon"/> -->
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
