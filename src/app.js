04c5d2fe37315bc142494fe6cf10a82fd37ea47c
Ambler <allinlbj@gmail.com>
2019-05-24 18:34:42 +0800

    使用 npm run test 和 npm run dev-test 自动化测试

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
@@ -23,83 +23,92 @@ chai.use(spies)
 
 const expect = chai.expect
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
+try {
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    vm.$el.remove() //有必要remove这个家伙吗？模板可不页面里边呀！难道这样做稳妥一点吗？
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount()
+    // 传给spy()的回调函数还是会被调用执行的！
+    // spy就是个mock，通过spy是否被调用，来断定spy()里边那个回调函数是否被调用了！
+    let spy = chai.spy(function () { })
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
+    vm.$el.remove()
+    vm.$destroy()
+  }
+} catch (error) {
+  window.errors = [error]
+} finally {
+  // 如果windows.errors存在就把全部的error给打出来！如果什么都没有打出来，那么就没有错！
+  window.errors && window.errors.forEach((error) => {
+    console.error(error.message)
   })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-settings')
-  vm.$el.remove() //有必要remove这个家伙吗？模板可不页面里边呀！难道这样做稳妥一点吗？
-  vm.$destroy()
-}  
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}  
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-} 
-{  
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPosition: 'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  })
-  vm.$mount()
-  // 传给spy()的回调函数还是会被调用执行的！
-  // spy就是个mock，通过spy是否被调用，来断定spy()里边那个回调函数是否被调用了！
-  let spy = chai.spy(function(){})
-  vm.$on('click', spy)
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-  vm.$el.remove()
-  vm.$destroy()
 }
---
+++ test/button.test.js
@@ -0,0 +1,84 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Button from '../src/button'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Button', () => {
+  it('存在.', () => {
+    // 只要Button不是个falsy值，那么它就是存在的！
+    expect(Button).to.exist
+  })
+  it('可以设置icon.', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    }).$mount()
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+    vm.$destroy()
+  })
+  it('可以设置loading.', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    }).$mount()
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    vm.$destroy()
+  })
+  it('icon 默认的 order 是 1', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    // 那到icon计算后的order属性值期待（expect）等于1
+    expect(getComputedStyle(icon).order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('设置 iconPosition 可以改变 order', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('点击 button 触发 click 事件', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      }
+    }).$mount()
+    // @click="$emit('click')"，如果你在button.vue的button元素上直接这样写@click="",那么这个测试用例就不通过了，否则
+    // 我们的测试用例写错了
+    const callback = sinon.fake();
+    // g-button组件被点击了
+    vm.$on('click', callback)
+    // button元素被点击了
+    vm.$el.click()
+    expect(callback).to.have.been.called
+
+  })
+})
