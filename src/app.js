890ed0cf2967fe625e93d3ada32eefad638eaa1d
Ambler <allinlbj@gmail.com>
2019-07-02 11:12:18 +0800

    添加 input 的测试用例

--- src/app.js
+++ src/app.js
@@ -15,6 +15,21 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false
+  },
+  created() {
+    // 自定义change事件，3s后触发，至此inputChange就会被执行。
+    // 去掉index.html之前的其它input组件测试代码，是为了保证在 #app里边找到唯一的input元素，好让下边的代码执行。
+    setTimeout(() => {
+      let event = new Event('change');
+      let inputElement = this.$el.querySelector('input')
+      inputElement.dispatchEvent(event)
+      console.log('hi')
+    }, 3000)
+  },
+  methods: {
+    inputChange(e) {
+      console.log(e)
+    }
   }
 })
 
