9bd95bf7b9fae52ebf25e089852581cd9965d8b9
Ambler <allinlbj@gmail.com>
2019-07-10 11:36:08 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="col" :class="[`span && col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2 +'px', paddingRight: gutter/2 + 'px'}"
-  >
-  <div style="border: 1px solid green; height: 100px;">
+  <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
-  </div>
   </div>
 </template>
 
@@ -24,11 +20,20 @@ export default {
       gutter: 0
     }
   },
-  created() {
-    console.log('col created')
-  },
-  mounted() {
-    console.log('col mounted')
+  computed: {
+    colClass() {
+      let {span, offset} = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
+    }
   }
   
 }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -12,23 +12,23 @@ export default {
       type: [Number, String]
     }
   },
-  created() {
-    console.log('row created')
+  computed: {
+    rowStyle() {
+      let {gutter} = this
+      return {
+        marginLeft: -gutter / 2 + 'px',
+        marginRight: -gutter / 2 + 'px'
+      }
+    }
   },
   mounted() {
-    console.log('row mounted')
-    console.log(this.$children)
     this.$children.forEach((vm) => {
       vm.gutter = this.gutter
     })
   }
 };
 
-// 关于对creaded 和 mounted 这两个钩子 的理解
-var div = document.createElement('div') // created
-var childDiv = document.createElement('div') // child created
-div.appendChild(childDiv) // child mounted
-document.body.appendChild(div) // mounted
+
 </script>
 
 <style lang="scss" scoped>
