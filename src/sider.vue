8c858cf5e4002ba57b426dac53fa171af23041d2
Ambler <allinlbj@gmail.com>
2019-07-17 18:37:31 +0800

    为 sider 添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+    <transition name="slide">
+      <div class="sider" v-if="visible" key="close">
+        <slot></slot>
+        <button @click="visible=false">close</button>
+      </div>
+      <div class="sider-show" v-else key="show">
+        <button @click="visible=true">show</button>
+      </div>
+    </transition>
 </template>
 
 <script>
 export default {
-  name: 'WarmSider'
-}
+  name: "WarmSider",
+  data() {
+    return {
+      visible: true
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .sider {
-    
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
   }
+}
+.sider-show {
+  position: fixed;
+  top: calc(50vh - 0.5em);
+  left: 0;
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all .5s;
+}
+// .slide-enter, .slide-leave-to {
+//   margin-left: -150px; //这个得根据用户设置sider的宽度来确定
+// }
 </style>
 
