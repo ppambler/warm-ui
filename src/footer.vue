dc55f8704151d15052dce65898e2201804622f9b
Ambler <allinlbj@gmail.com>
2019-07-17 13:26:56 +0800

    layout 组件基本功能完成

---
+++ src/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+    
+  }
+</style>
+
---
+++ src/sider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WarmSider'
+}
+</script>
+
+<style lang="scss" scoped>
+  .sider {
+    
+  }
+</style>
+
