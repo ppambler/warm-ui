275edb41568963ade78840a8818f18e821571e68
Ambler <allinlbj@gmail.com>
2019-07-22 17:17:21 +0800

    添加 toast 和 plugin

---
+++ src/toast.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WarmToast' 
+}
+</script>
+<style lang="scss" scoped>
+  $font-size: 14px;
+  $toast-height: 40px;
+  $toast-bgc: rgba(0, 0, 0, 0.75);
+  .toast {
+    font-size: $font-size;
+    height: $toast-height;
+    line-height: 1.8;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    color: white;
+    align-items: center;
+    background-color: $toast-bgc;
+    border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+    padding: 0 16px;
+  }
+</style>
+
