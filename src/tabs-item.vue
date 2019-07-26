4f64951a1e0d2109d0ad0776617b8bbb9227413e
Ambler <allinlbj@gmail.com>
2019-07-26 09:30:41 +0800

    添加 tabs 相关组件，并按需求添加基本的 props

---
+++ src/tabs-item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tabs-item"></div>
+</template>
+
+<script>
+export default {
+  name: "WarmTabsItem",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tabs-item {
+}
+</style>
