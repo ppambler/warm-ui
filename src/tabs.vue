f1918f3ada36818a1d3506f806b28435a400c8fd
Ambler <allinlbj@gmail.com>
2019-08-18 15:48:16 +0800

    重构 tabs.vue （一函数就5行代码）

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,25 +32,31 @@ export default {
     }
   },
   mounted() {
-    if(this.$children.length === 0) {
-      console && console.warn && console.warn('tabs 的子组件应该是 tabs-head 和 tabs-nav，但你没有写子组件')
-    }
-    this.$children.forEach((vm)=>{
-      if(vm.$options.name === 'WarmTabsHead') {
-        vm.$children.forEach((childVm)=>{
-          if(childVm.$options.name === 'WarmTabsItem'
-            && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+    this.checkChildren()
+    this.selectTab()
+  },
+  methods: {
+    checkChildren() {
+      if(this.$children.length === 0) {
+        console && console.warn && console.warn('tabs 的子组件应该是 tabs-head 和 tabs-nav，但你没有写子组件')
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach((vm)=>{
+        if(vm.$options.name === 'WarmTabsHead') {
+          vm.$children.forEach((childVm)=>{
+            if(childVm.$options.name === 'WarmTabsItem'
+              && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-  .tabs {
 
-  }
 </style>
