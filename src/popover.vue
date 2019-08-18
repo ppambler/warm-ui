709328bf83a5434faeb0cdcd219d8d22a38a45a0
Ambler <allinlbj@gmail.com>
2019-08-18 16:39:09 +0800

    为 popover 添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
     </span>
   </div>
@@ -25,9 +25,12 @@ export default {
       this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
     },
     onClickDocument(e) {
-      if( this.$refs.popover && ( this.$refs.popover.contains(e.target) || this.$refs.contentWrapper.contains(e.target) )) { 
-        return 
-      }
+      if (this.$refs.popover &&
+            (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+      ) { return }
+      if (this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+      ) { return }
       this.close()
     },
     open() {
@@ -55,15 +58,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   position: relative;
-  display: inline-flex;
+  display: inline-block;
   vertical-align: top;
 }
 .content-wrapper {
   position: absolute;
-  box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-  border: 1px solid red;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
+  background-color: white;
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: .5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  &::before, &::after {
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 10px;
+  }
+  &::before {
+    border-top-color: black;
+    top: 100%;
+  }
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
