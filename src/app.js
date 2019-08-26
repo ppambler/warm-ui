d1a8757533b7a74e4055410c8b5369bdf91d7689
Ambler <allinlbj@gmail.com>
2019-08-26 22:16:12 +0800

    完成 collapse 的基本样式 和 基本功能

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ import TabsBody from './tabs-body'
 import TabsItem from './tabs-item'
 import TabsPane from './tabs-pane'
 import Popover from './popover'
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -38,6 +41,8 @@ Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
 Vue.component('g-popover', Popover)
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
 
 new Vue({
   el: '#app',
@@ -60,7 +65,7 @@ new Vue({
       this.showToast('bottom')
     },
     showToast(position) {
-      this.$toast(`你的智商目前为${parseInt(Math.random() * 100)}。你的智商需要充值！`,{
+      this.$toast(`你的智商目前为${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
         position,
         closeButton: {
           text: '已充值',
---
+++ src/collapse-item.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="collapseItem">
+    <div class="title" @click="open=!open">{{title}}</div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WarmCollapseItem",
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      open: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+$title-bgc: #fafafa;
+$content-bgc: #fff;
+.collapseItem {
+   > .title {
+    display: flex;
+    align-items: center;
+    min-height: 32px;
+    padding: 0 8px;
+    margin-top: -1px;
+    background-color: $title-bgc;
+    border-top: 1px solid $grey;
+    border-bottom: 1px solid $grey;
+  }
+  &:first-child {
+     > .title {
+      border-top-right-radius: $border-radius;
+      border-top-left-radius: $border-radius;
+    }
+  }
+  &:last-child {
+     > .title:last-child {
+      border-bottom: none;
+      border-bottom-right-radius: $border-radius;
+      border-bottom-left-radius: $border-radius;
+    }
+     > .content {
+      border-bottom-right-radius: $border-radius;
+      border-bottom-left-radius: $border-radius;
+    }
+  }
+   > .content {
+    padding: 8px;
+    background-color: $content-bgc;
+  }
+}
+</style>
