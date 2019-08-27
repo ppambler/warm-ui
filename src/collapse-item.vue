4a96198637bfa8b81b4b69b9b08c72df199cb29a
Ambler <allinlbj@gmail.com>
2019-08-27 17:25:42 +0800

    可以设置默认的 selected，自带single功能

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -14,6 +14,10 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
   },
   data() {
@@ -23,9 +27,11 @@ export default {
   },
   inject: ['eventBus'],
   mounted() {
-    this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
-      if(vm != this) {
+    this.eventBus && this.eventBus.$on('update:selected',(name)=>{
+      if(name != this.name) {
         this.close()
+      } else {
+        this.show()
       }
     })
   },
@@ -35,11 +41,14 @@ export default {
         this.open = false
       } else {
         this.open = true
-        this.eventBus && this.eventBus.$emit('update:selected',this)
+        this.eventBus && this.eventBus.$emit('update:selected',this.name)
       }
     },
     close() {
       this.open = false
+    },
+    show() {
+      this.open = true
     }
   }
 };
--- src/collapse.vue
+++ src/collapse.vue
@@ -12,6 +12,9 @@ export default {
     single: {
       type: Boolean,
       default: false
+    },
+    selected: {
+      type: String,
     }
   },
   data() {
@@ -20,11 +23,15 @@ export default {
     };
   },
   provide() {
-    if (this.single) {
       return {
         eventBus: this.eventBus
       };
-    }
+  },
+  mounted() {
+    this.eventBus.$emit('update:selected',this.selected)
+    this.eventBus.$on('updated:selected',(name)=>{
+      this.$emit('update:selected',name)
+    })
   }
 };
 </script>
