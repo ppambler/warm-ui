e0e33a2ff964c32c39b0c8e05c0ec95f1a4017f4
Ambler <allinlbj@gmail.com>
2019-08-30 11:01:51 +0800

    完成 icon doc

---
+++ docs/.vuepress/components/General/Icon1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <div class="icon1">
+      <w-icon class="icon" name="smile"></w-icon>
+      <w-icon class="icon" name="check-circle"></w-icon>
+      <w-icon class="icon" name="sync"></w-icon>
+      <w-icon class="icon" name="comment"></w-icon>
+      <w-icon class="icon" name="bug"></w-icon>
+      <w-icon class="icon" name="edit-square"></w-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "../../../../src/icon";
+export default {
+  name: "icon1",
+  components: { "w-icon": Icon }
+};
+</script>
+
+<style>
+.wrapper {
+  margin-top: 1.5em;
+}
+/* 没有这个，这几个icon的布局就是一条垂直的棍子 */
+.icon1 {
+  display: inline;
+}
+/* 默认是1em，为了预览效果好点，放大1em */
+.icon1 > .icon {
+  width: 2em;
+  height: 2em;
+}
+</style>
--- docs/components/general/icon.md
+++ docs/components/general/icon.md
@@ -2,3 +2,25 @@
 title: Icon 图标
 ---
 # Icon 图标
+
+> 语义化的矢量图形。
+
+## 简单用法
+
+### 预览
+
+<ClientOnly>
+  <General-Icon1 />
+</ClientOnly>
+
+### 代码
+
+```html
+<g-icon name="smile"></g-icon>
+<g-icon name="check-circle"></g-icon>
+<g-icon name="sync"></g-icon>
+<g-icon name="comment"></g-icon>
+<g-icon name="bug"></g-icon>
+<g-icon name="edit-square"></g-icon>
+```
+
